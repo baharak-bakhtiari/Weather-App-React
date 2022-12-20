5cb35edfb761c602d0ce21873a6c2bd7bb662f70
The-little-spring <100764083+The-little-spring@users.noreply.github.com>
2022-12-21 02:05:18 +0330

    add GitHub link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [temp, setTemp] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [icon, setIcon] = useState(null);
+  const [description, setDescription] = useState(null);
+
+  function showForecast(response) {
+    setLoaded(true);
+    setTemp(Math.round(response.data.main.temp));
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    setDescription(response.data.weather[0].description);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "de2c40e370d58e257faf07ba4ea95840";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(showForecast);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="type a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <a
+        href="https://github.com/The-little-spring/Weather-App-React"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github link
+      </a>
     </div>
   );
-}
 
-export default App;
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <div>Weather is {city}:</div>
+        <ul>
+          <li>temperature: {temp}℃</li>
+          <li>{description}</li>
+          <li>humidity: {humidity}%</li>
+          <li>Wind: {wind}km/h</li>
+          <li>
+            <img src={icon} alt={description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="app">
+      <h3>Weather Search engine</h3>
+      <App />
+    </div>
   </React.StrictMode>
 );
 
